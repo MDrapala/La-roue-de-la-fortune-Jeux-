d00c943640b92efd2aa7446e00d597a74cf3f946
MDrapala <drapalamathieu95@gmail.com>
2023-09-14 09:15:49 +0200

    clean code

--- main.c
+++ main.c
@@ -3,91 +3,77 @@
 //  Copyright © 2019 Mathieu Drapala. All rights reserved.
 //
 
-//includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char *argv[])
-{
-    //Mes variables
-    int menu;
-    int play;
-    int replay;
-    
-    int nombre;
-    int nombreMystere = 0;
-    
-    int MIN = 0;
-    int nb1 = 100;
-    int nb2 = 1000;
-    int nb3 = 10000;
-    
-    do
-    {
-        replay = 0;
-        
-        //Menu
-        printf("== Bienvenue sur Le jeux Plus ou Moins ! ==\n\n");
-        printf("1 - Entrer un nombre, entre 1 et 100 ! \n");
-        printf("2 - Entrer un nombre, entre 1 et 1000 ! \n");
-        printf("3 - Entrer un nombre, entre 1 et 10000 ! \n");
-        printf("=> Entrer le nombre de votre choix ! ");
-        scanf("%d",&menu);
-        
-        // Tire un nombre aléatoire
-        srand(time(NULL));
-        
-        switch(menu)
-        {
-            case 1:
-                nombreMystere = (rand() % (nb1 - MIN + 1)) + MIN;
-                printf("=> Bonne chance ! \n ");
-                break;
-            case 2:
-                nombreMystere = (rand() % (nb2 - MIN + 1)) + MIN;
-                printf("=> Bonne chance ! \n");
-                break;
-            case 3:
-                nombreMystere = (rand() % (nb3 - MIN + 1)) + MIN;
-                printf("=> Bonne chance ! \n");
-                break;
-            default:
-                printf("Veuillez saisir un nombre Valide !\n");
-        }
-        
-        //boucle avec les conditions
-        for(nombre = 0; nombre != nombreMystere;)
-        {
-            if(nombre > nombreMystere)
-            {
-                printf("Saisir un nombre inférieur à %d \n", nombre);
-                scanf("%d", &nombre);
-            }
-            else if(nombre < nombreMystere)
-            {
-                printf("Saisir un nombre supérieur à %d \n", nombre);
-                scanf("%d", &nombre);
-            }
-        }
-        
-        //Si j'ai trouver le résultat
-        printf("Félicitation vous avez trouver le nombre mystére qui est : %d\n", nombreMystere);
-        
-        printf("Voulez-vous rejouer \n");
-        printf("1. oui \n");
-        printf("2. non \n");
-        scanf("%d",&play);
-        switch(play){
-            case 1:
-                printf("\n");
-                replay = 1;
-                break;
-            case 2:
-                printf("Au Revoir !\n");
-                break;
-        }
-    }
-    while (replay == 1);
-}
+int main(int argc, char * argv[]) {
+  int menu;
+  int play;
+  int replay;
 
+  int nombre;
+  int nombreMystere = 0;
+
+  int MIN = 0;
+  int nb1 = 100;
+  int nb2 = 1000;
+  int nb3 = 10000;
+
+  do {
+    replay = 0;
+
+    printf("== Bienvenue sur Le jeux Plus ou Moins ! ==\n\n");
+    printf("1 - Entrer un nombre, entre 1 et 100 !\n");
+    printf("2 - Entrer un nombre, entre 1 et 1000 !\n");
+    printf("3 - Entrer un nombre, entre 1 et 10000 !\n");
+    printf("=> Entrer le nombre de votre choix ! ");
+    scanf("%d", & menu);
+
+    srand(time(NULL));
+
+    switch (menu) {
+    case 1:
+      nombreMystere = (rand() % (nb1 - MIN + 1)) + MIN;
+      printf("=> Bonne chance ! \n ");
+      break;
+    case 2:
+      nombreMystere = (rand() % (nb2 - MIN + 1)) + MIN;
+      printf("=> Bonne chance ! \n");
+      break;
+    case 3:
+      nombreMystere = (rand() % (nb3 - MIN + 1)) + MIN;
+      printf("=> Bonne chance ! \n");
+      break;
+    default:
+      printf("Veuillez saisir un nombre Valide !\n");
+    }
+
+    for (nombre = 0; nombre != nombreMystere;) {
+      if (nombre > nombreMystere) {
+        printf("Saisir un nombre inférieur à %d \n", nombre);
+        scanf("%d", & nombre);
+      } else if (nombre < nombreMystere) {
+        printf("Saisir un nombre supérieur à %d \n", nombre);
+        scanf("%d", & nombre);
+      }
+    }
+
+    printf("Félicitations, vous avez trouvé le nombre mystère, qui est : %d\n", nombreMystere);
+
+    printf("Voulez-vous rejouer \n");
+    printf("1. oui \n");
+    printf("2. non \n");
+    scanf("%d", & play);
+    switch (play) {
+    case 1:
+      printf("\n");
+      replay = 1;
+      break;
+    case 2:
+      printf("Au Revoir !\n");
+      break;
+    }
+  }
+  while (replay == 1);
+}
